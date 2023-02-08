4be412a7f4e1492f16fc665fc557dd4553034787
Sara Raley <sara.m.raley@gmail.com>
2023-02-08 13:13:40 -0700

    adds background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { Animals } from "./Animals";
 
 function App() {
   const title = "";
-  const animalFacts = <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>;
+
+  const background = <img className="background" src="src/assets/1166781.webp" alt="ocean" />;
+  const animalFacts = (
+    <div>
+      <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
+      {background}
+    </div>
+  );
 
   return <div>{animalFacts}</div>;
 }
