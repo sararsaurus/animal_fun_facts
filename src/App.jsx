45a938eef0b9470fc197bf277a06e41ddc008dd5
Sara Raley <sara.m.raley@gmail.com>
2023-02-13 14:24:08 -0700

    adds notes and future steps

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import { Animals } from "./Animals";
 
 function App() {
+  // console.log(Animals);
+  // Animals is an object
   const displayFact = (e) => {
+    // console.log(e.target);
+    // visually checking what we're doing
     const selectedAnimal = e.target.alt;
+    // read more and fill in about e.target
     const animalInfo = Animals[selectedAnimal];
+    // access data.config
+    // console.log(animalInfo);
+    // practice copying an object in the console. This console.log gave us animalInfo and then we can right-click and "copy object" and then we could experiment with it in a console. We set it to a variable first and then essentially can make it work like the rails console.
     const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+    // pulling a random fact
     const funFact = animalInfo.facts[optionIndex];
+    // picking fact that was grabbed on line 14
     document.getElementById("fact").innerHTML = funFact;
+    // console.log(document);
+    // gives us the DOM -- it is finding something in the DOM that has the id of "fact"
+    // .innerHTML is adding the html to be funFact, which needs to be randomly generated
   };
 
   const title = "";
 
   const images = [];
   for (const animal in Animals) {
+    // try to refactor as .map
     images.push(
       <img
         key={animal}
@@ -26,6 +40,10 @@ function App() {
       />
     );
   }
+
+  // find other examples to take this a step further - change colors when you click or certain images disappear or a certain image grows
+
+  // then go back and refactor to do it in a more simple way
 
   const animalFacts = (
     <div>
@@ -42,3 +60,8 @@ function App() {
 }
 
 export default App;
+
+// https://reactjs.org/docs/events.html
+// https://www.google.com/search?q=react+e.target.value&oq=react+e.target&aqs=chrome.0.0i512j69i57j0i512l8.2430j0j7&sourceid=chrome&ie=UTF-8
+// https://rossbulat.medium.com/react-native-working-with-objects-aff45bff6832
+// https://djoech.medium.com/functional-vs-class-components-in-react-231e3fbd7108#:~:text=A%20functional%20component%20is%20just,which%20returns%20a%20React%20element.%5C
